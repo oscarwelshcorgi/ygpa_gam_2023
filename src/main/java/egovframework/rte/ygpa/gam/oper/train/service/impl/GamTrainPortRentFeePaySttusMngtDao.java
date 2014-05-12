667030a660ae391eb8caec7aaf10ee1bf80521ac
yesdaisy <yesdaisy@55a9a2a8-cf3b-443f-8c2c-fc4ace15ebcd>
2014-05-12 12:37:18 +0000

    수정작업

--- src/main/java/egovframework/rte/ygpa/gam/oper/train/service/impl/GamTrainPortRentFeePaySttusMngtDao.java
+++ src/main/java/egovframework/rte/ygpa/gam/oper/train/service/impl/GamTrainPortRentFeePaySttusMngtDao.java
@@ -1,10 +1,13 @@
 package egovframework.rte.ygpa.gam.oper.train.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
 import egovframework.rte.cmmn.dataaccess.YGPAAbstractDAO;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import egovframework.rte.ygpa.gam.oper.train.service.GamTrainPortRentArrrgMngtVO;
 import egovframework.rte.ygpa.gam.oper.train.service.GamTrainPortRentFeePaySttusMngtVO;
 
 /**
@@ -21,35 +24,88 @@ import egovframework.rte.ygpa.gam.oper.train.service.GamTrainPortRentFeePaySttus
  */
 @Repository("gamTrainPortRentFeePaySttusMngtDao")
 public class GamTrainPortRentFeePaySttusMngtDao extends YGPAAbstractDAO {
-	
+
 	/**
-	 * 철송장임대납부현황관리 목록을 조회한다.
+	 * 자산임대료납부관리 목록을 조회한다.
 	 * @param searchMap - 조회할 정보가 담긴 Map
-	 * @return 철송장임대납부현황관리 목록
+	 * @return 자산임대료납부관리 목록
 	 * @exception Exception
 	 */
-    public List selectTrainPortRentFeePaySttusMngtList(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
-        return list("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePaySttusMngtList_D", searchVO);
+    public List selectTrainPortRentFeePayDtlsList(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+        return list("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsList_D", searchVO);
     }
 
     /**
-	 * 철송장임대납부현황관리 목록 총 갯수를 조회한다.
+	 * 자산임대료납부관리 목록 총 갯수를 조회한다.
 	 * @param searchMap - 조회할 정보가 담긴 Map
-	 * @return 철송장임대납부현황관리 목록 총 갯수
+	 * @return 자산임대료납부관리 목록 총 갯수
 	 * @exception
 	 */
-    public int selectTrainPortRentFeePaySttusMngtListTotCnt(GamTrainPortRentFeePaySttusMngtVO searchVO) {
-        return (Integer)getSqlMapClientTemplate().queryForObject("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePaySttusMngtListTotCnt_S", searchVO);
+    public int selectTrainPortRentFeePayDtlsListTotCnt(GamTrainPortRentFeePaySttusMngtVO searchVO) {
+        return (Integer)getSqlMapClientTemplate().queryForObject("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsListTotCnt_S", searchVO);
     }
-    
+
+    public EgovMap selectTrainPortRentFeePayDtlsMngtDetailMstPk(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+		return (EgovMap) selectByPk("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsMngtDetailMaster_D", searchVO);
+    }
+
+    public List selectTrainPortRentFeePayDtlsMngtDetailList(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+        return list("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsMngtDetailList_D", searchVO);
+    }
+
+    public EgovMap selectTrainPortRentFeePayDtlsMngtDetailSumPk(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+		return (EgovMap) selectByPk("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsMngtDetailSum_D", searchVO);
+    }
+
     /**
 	 * 자료수, 사용료, 부가세, 고지액을 조회한다.
 	 * @param searchMap - 조회할 정보가 담긴 Map
-	 * @return 철송장임대납부현황관리 목록
+	 * @return 자산임대료납부관리 목록
 	 * @exception Exception
 	 */
-	public GamTrainPortRentFeePaySttusMngtVO selectTrainPortRentFeePaySttusMngtSum(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
-		return (GamTrainPortRentFeePaySttusMngtVO) selectByPk("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePaySttusMngtSum_S", searchVO);
+	public GamTrainPortRentFeePaySttusMngtVO selectTrainPortRentFeePayDtlsSum(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+		return (GamTrainPortRentFeePaySttusMngtVO) selectByPk("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsSum_S", searchVO);
 	}
-	
+
+    public EgovMap selectNticArrrgDetail(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+		return (EgovMap) selectByPk("gamTrainPortRentFeePaySttusMngtDao.selectNticArrrgDetail_S", searchVO);
+    }
+
+
+	/**
+	 * 고지금액합계, 수납금액합계
+	 * @param searchMap - 조회할 정보가 담긴 Map
+	 * @return vo
+	 * @exception Exception
+	 */
+	public GamTrainPortRentFeePaySttusMngtVO selectTrainPortRentFeePayDtlsMngtSum(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+		return (GamTrainPortRentFeePaySttusMngtVO) selectByPk("gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsMngtSum_S", searchVO);
+	}
+
+	public List<?> selectNticArrrgList(GamTrainPortRentArrrgMngtVO searchVO)
+			throws Exception {
+        return list("gamTrainPortRentFeePaySttusMngtDao.selectNticArrrgList_D", searchVO);
+	}
+
+	public int selectNticArrrgListTotCnt(GamTrainPortRentArrrgMngtVO searchVO)
+			throws Exception {
+        return (Integer)getSqlMapClientTemplate().queryForObject("gamTrainPortRentFeePaySttusMngtDao.selectNticArrrgListTotCnt_S", searchVO);
+	}
+
+	public List mergeNticArrrgList(Map mergeMap) throws Exception {
+		return this.merge(mergeMap, "gamTrainPortRentFeePaySttusMngtDao.insertAssetPhoto_S", "gamTrainPortRentFeePaySttusMngtDao.updateAssetPhoto_S", "gamTrainPortRentFeePaySttusMngtDao.deleteAssetPhoto_S");
+	}
+
+	public int updateTrainPortRentFeePayDtlsMngtList() throws Exception {
+		return update("gamTrainPortRentFeePaySttusMngtDao.updateTrainPortRentFeePayDtlsMngtList", null);
+	}
+
+	/**
+	 * 연체 등록
+	 * @param map
+	 * @throws Exception
+	 */
+	public void updateRevArrrgAnlrveBndeRcvdTp(Map map) throws Exception {
+		this.update("gamTrainPortRentFeePaySttusMngtDao.updateRevArrrgAnlrveBndeRcvdTp", map);
+	}
 }
--- src/main/java/egovframework/rte/ygpa/gam/oper/train/service/impl/GamTrainPortRentFeePaySttusMngtServiceImpl.java
+++ src/main/java/egovframework/rte/ygpa/gam/oper/train/service/impl/GamTrainPortRentFeePaySttusMngtServiceImpl.java
@@ -1,6 +1,9 @@
 package egovframework.rte.ygpa.gam.oper.train.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -9,6 +12,9 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import egovframework.rte.fdl.cmmn.AbstractServiceImpl;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import egovframework.rte.ygpa.gam.cmmn.fclty.service.GamNticRequestMngtService;
+import egovframework.rte.ygpa.gam.oper.train.service.GamTrainPortRentArrrgMngtVO;
 import egovframework.rte.ygpa.gam.oper.train.service.GamTrainPortRentFeePaySttusMngtService;
 import egovframework.rte.ygpa.gam.oper.train.service.GamTrainPortRentFeePaySttusMngtVO;
 
@@ -28,39 +34,151 @@ import egovframework.rte.ygpa.gam.oper.train.service.GamTrainPortRentFeePaySttus
 
 public class GamTrainPortRentFeePaySttusMngtServiceImpl  extends AbstractServiceImpl implements GamTrainPortRentFeePaySttusMngtService {
 
+	@Resource(name="gamNticRequestMngtService")
+    private GamNticRequestMngtService gamNticRequestMngtService;
+	
 	@Resource(name="gamTrainPortRentFeePaySttusMngtDao")
     private GamTrainPortRentFeePaySttusMngtDao gamTrainPortRentFeePaySttusMngtDao;
 
 	protected Log log = LogFactory.getLog(this.getClass());
 
 	/**
-	 * 철송장임대납부현황관리 목록을 조회한다.
+	 * 자산임대료납부관리 목록을 조회한다.
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return list
 	 * @exception Exception
 	 */
-    public List selectTrainPortRentFeePaySttusMngtList(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
-        return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePaySttusMngtList(searchVO);
+    public List selectTrainPortRentFeePayDtlsList(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+        return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsList(searchVO);
     }
 
     /**
-	 * 철송장임대납부현황관리 목록 총 갯수를 조회한다.
+	 * 자산임대료납부관리 목록 총 갯수를 조회한다.
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return cnt
 	 * @exception
 	 */
-    public int selectTrainPortRentFeePaySttusMngtListTotCnt(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
-		return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePaySttusMngtListTotCnt(searchVO);
+    public int selectTrainPortRentFeePayDtlsListTotCnt(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+		return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsListTotCnt(searchVO);
 	}
 
     /**
 	 * 자료수, 사용료, 부가세, 고지액을 조회한다.
 	 * @param searchVO - 조회할 정보가 담긴 VO
-	 * @return 철송장임대납부현황관리목록
+	 * @return 자산임대료납부관리목록
 	 * @exception Exception
 	 */
-    public GamTrainPortRentFeePaySttusMngtVO selectTrainPortRentFeePaySttusMngtSum(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
-        return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePaySttusMngtSum(searchVO);
+    public GamTrainPortRentFeePaySttusMngtVO selectTrainPortRentFeePayDtlsSum(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+        return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsSum(searchVO);
     }
+
+
+    /**
+	 * 고지금액합계, 수납금액합계
+	 * @param searchVO - 조회할 정보가 담긴 VO
+	 * @return vo
+	 * @exception Exception
+	 */
+    public GamTrainPortRentFeePaySttusMngtVO selectTrainPortRentFeePayDtlsMngtSum(GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+        return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsMngtSum(searchVO);
+    }
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePayDtlsMngtService#selectNticArrrgList(egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePaySttusMngtVO)
+	 */
+	@Override
+	public List<?> selectNticArrrgList(GamTrainPortRentArrrgMngtVO searchVO)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return gamTrainPortRentFeePaySttusMngtDao.selectNticArrrgList(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePayDtlsMngtService#selectNticArrrgListTotCnt(egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePaySttusMngtVO)
+	 */
+	@Override
+	public int selectNticArrrgListTotCnt(GamTrainPortRentArrrgMngtVO searchVO)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return gamTrainPortRentFeePaySttusMngtDao.selectNticArrrgListTotCnt(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePayDtlsMngtService#mergeErpGisAssetCodeMngt(java.util.Map)
+	 */
+	@Override
+	public List mergeNticArrrgListMngt(Map mergeMap) throws Exception {
+
+        ArrayList arraylistCU = (ArrayList)mergeMap.get("CU");
+        HashMap[] hmCU = (HashMap[])arraylistCU.toArray(new HashMap[arraylistCU.size()]);
+        Map result;
+
+//        Integer photoSeq=1;
+
+//		if(hmCU.length>0) photoSeq=gamErpGisAssetPhotoMngtDao.selectGamAssetPhotoMaxSeq(hmCU[0]);
+        //수정처리 & 입력처리
+        for (int i=0; i<hmCU.length; i++) {
+        	if ("I".equals(hmCU[i].get("_updtId"))) {
+//            	log.debug("#photoeq : "+photoSeq.toString());
+            }
+        	else if("U".equals(hmCU[i].get("_updtId"))){
+        		// 연체 세입 선택 목록
+
+        	}
+            else {
+            	log.debug("unknown RowStatus ["+i+"] : "+hmCU[i].get("_updtId"));
+            }
+        }
+
+
+
+		// TODO Auto-generated method stub
+		return gamTrainPortRentFeePaySttusMngtDao.mergeNticArrrgList(mergeMap);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePayDtlsMngtService#updateTrainPortRentFeePayDtlsMngtList()
+	 */
+	@Override
+	public int updateTrainPortRentFeePayDtlsMngtList() throws Exception {
+		return gamTrainPortRentFeePaySttusMngtDao.updateTrainPortRentFeePayDtlsMngtList();
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePayDtlsMngtService#selectTrainPortRentFeePayDtlsMngtDetailList(egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePaySttusMngtVO)
+	 */
+	@Override
+	public List selectTrainPortRentFeePayDtlsMngtDetailList(
+			GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+        return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsMngtDetailList(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePayDtlsMngtService#selectTrainPortRentFeePayDtlsMngtDetailMstPk(egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePaySttusMngtVO)
+	 */
+	@Override
+	public EgovMap selectTrainPortRentFeePayDtlsMngtDetailMstPk(
+			GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+		return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsMngtDetailMstPk(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePayDtlsMngtService#selectTrainPortRentFeePayDtlsMngtDetailSumPk(egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePaySttusMngtVO)
+	 */
+	@Override
+	public EgovMap selectTrainPortRentFeePayDtlsMngtDetailSumPk(
+			GamTrainPortRentFeePaySttusMngtVO searchVO) throws Exception {
+		return gamTrainPortRentFeePaySttusMngtDao.selectTrainPortRentFeePayDtlsMngtDetailSumPk(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePayDtlsMngtService#selectNticArrrgDetail(egovframework.rte.ygpa.gam.asset.rent.service.GamTrainPortRentFeePaySttusMngtVO)
+	 */
+	@Override
+	public Map selectNticArrrgDetail(GamTrainPortRentFeePaySttusMngtVO searchVO)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return gamTrainPortRentFeePaySttusMngtDao.selectNticArrrgDetail(searchVO);
+	}
 
 }
