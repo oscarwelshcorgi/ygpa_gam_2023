3a9e89637e7851b9485d936f7b6299decf3617c3
yesdaisy <yesdaisy@55a9a2a8-cf3b-443f-8c2c-fc4ace15ebcd>
2014-05-12 12:36:36 +0000

    수정작업

--- src/main/java/egovframework/rte/ygpa/gam/oper/center/service/impl/GamMarineCenterRentNticMngtDao.java
+++ src/main/java/egovframework/rte/ygpa/gam/oper/center/service/impl/GamMarineCenterRentNticMngtDao.java
@@ -1,10 +1,13 @@
 package egovframework.rte.ygpa.gam.oper.center.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
 import egovframework.rte.cmmn.dataaccess.YGPAAbstractDAO;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import egovframework.rte.ygpa.gam.oper.center.service.GamMarineCenterRentArrrgMngtVO;
 import egovframework.rte.ygpa.gam.oper.center.service.GamMarineCenterRentNticMngtVO;
 
 /**
@@ -23,33 +26,86 @@ import egovframework.rte.ygpa.gam.oper.center.service.GamMarineCenterRentNticMng
 public class GamMarineCenterRentNticMngtDao extends YGPAAbstractDAO {
 	
 	/**
-	 * 마린센터임대료납부관리 목록을 조회한다.
+	 * 자산임대료납부관리 목록을 조회한다.
 	 * @param searchMap - 조회할 정보가 담긴 Map
-	 * @return 마린센터임대료납부관리 목록
+	 * @return 자산임대료납부관리 목록
 	 * @exception Exception
 	 */
-    public List selectMarineCenterRentNticList(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
-        return list("gamMarineCenterRentNticMngtDao.selectMarineCenterRentNticList_D", searchVO);
+    public List selectMarineCenterRentFeePayDtlsList(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+        return list("gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsList_D", searchVO);
     }
 
     /**
-	 * 마린센터임대료납부관리 목록 총 갯수를 조회한다.
+	 * 자산임대료납부관리 목록 총 갯수를 조회한다.
 	 * @param searchMap - 조회할 정보가 담긴 Map
-	 * @return 마린센터임대료납부관리 목록 총 갯수
+	 * @return 자산임대료납부관리 목록 총 갯수
 	 * @exception
 	 */
-    public int selectMarineCenterRentNticListTotCnt(GamMarineCenterRentNticMngtVO searchVO) {
-        return (Integer)getSqlMapClientTemplate().queryForObject("gamMarineCenterRentNticMngtDao.selectMarineCenterRentNticListTotCnt_S", searchVO);
+    public int selectMarineCenterRentFeePayDtlsListTotCnt(GamMarineCenterRentNticMngtVO searchVO) {
+        return (Integer)getSqlMapClientTemplate().queryForObject("gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsListTotCnt_S", searchVO);
     }
-    
+
+    public EgovMap selectMarineCenterRentFeePayDtlsMngtDetailMstPk(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+		return (EgovMap) selectByPk("gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsMngtDetailMaster_D", searchVO);
+    }
+
+    public List selectMarineCenterRentFeePayDtlsMngtDetailList(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+        return list("gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsMngtDetailList_D", searchVO);
+    }
+
+    public EgovMap selectMarineCenterRentFeePayDtlsMngtDetailSumPk(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+		return (EgovMap) selectByPk("gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsMngtDetailSum_D", searchVO);
+    }
+
     /**
 	 * 자료수, 사용료, 부가세, 고지액을 조회한다.
 	 * @param searchMap - 조회할 정보가 담긴 Map
-	 * @return 마린센터임대료납부관리 목록
+	 * @return 자산임대료납부관리 목록
 	 * @exception Exception
 	 */
-	public GamMarineCenterRentNticMngtVO selectMarineCenterRentNticSum(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
-		return (GamMarineCenterRentNticMngtVO) selectByPk("gamMarineCenterRentNticMngtDao.selectMarineCenterRentNticSum_S", searchVO);
+	public GamMarineCenterRentNticMngtVO selectMarineCenterRentFeePayDtlsSum(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+		return (GamMarineCenterRentNticMngtVO) selectByPk("gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsSum_S", searchVO);
 	}
-	
+
+    public EgovMap selectNticArrrgDetail(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+		return (EgovMap) selectByPk("gamMarineCenterRentNticMngtDao.selectNticArrrgDetail_S", searchVO);
+    }
+
+
+	/**
+	 * 고지금액합계, 수납금액합계
+	 * @param searchMap - 조회할 정보가 담긴 Map
+	 * @return vo
+	 * @exception Exception
+	 */
+	public GamMarineCenterRentNticMngtVO selectMarineCenterRentFeePayDtlsMngtSum(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+		return (GamMarineCenterRentNticMngtVO) selectByPk("gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsMngtSum_S", searchVO);
+	}
+
+	public List<?> selectNticArrrgList(GamMarineCenterRentArrrgMngtVO searchVO)
+			throws Exception {
+        return list("gamMarineCenterRentNticMngtDao.selectNticArrrgList_D", searchVO);
+	}
+
+	public int selectNticArrrgListTotCnt(GamMarineCenterRentArrrgMngtVO searchVO)
+			throws Exception {
+        return (Integer)getSqlMapClientTemplate().queryForObject("gamMarineCenterRentNticMngtDao.selectNticArrrgListTotCnt_S", searchVO);
+	}
+
+	public List mergeNticArrrgList(Map mergeMap) throws Exception {
+		return this.merge(mergeMap, "gamMarineCenterRentNticMngtDao.insertAssetPhoto_S", "gamMarineCenterRentNticMngtDao.updateAssetPhoto_S", "gamMarineCenterRentNticMngtDao.deleteAssetPhoto_S");
+	}
+
+	public int updateMarineCenterRentFeePayDtlsMngtList() throws Exception {
+		return update("gamMarineCenterRentNticMngtDao.updateMarineCenterRentFeePayDtlsMngtList", null);
+	}
+
+	/**
+	 * 연체 등록
+	 * @param map
+	 * @throws Exception
+	 */
+	public void updateRevArrrgAnlrveBndeRcvdTp(Map map) throws Exception {
+		this.update("gamMarineCenterRentNticMngtDao.updateRevArrrgAnlrveBndeRcvdTp", map);
+	}
 }
--- src/main/java/egovframework/rte/ygpa/gam/oper/center/service/impl/GamMarineCenterRentNticMngtServiceImpl.java
+++ src/main/java/egovframework/rte/ygpa/gam/oper/center/service/impl/GamMarineCenterRentNticMngtServiceImpl.java
@@ -1,6 +1,10 @@
 package egovframework.rte.ygpa.gam.oper.center.service.impl;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -8,7 +12,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.service.EgovProperties;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.com.utl.fcc.service.EgovDateUtil;
+import egovframework.com.utl.fcc.service.EgovStringUtil;
 import egovframework.rte.fdl.cmmn.AbstractServiceImpl;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import egovframework.rte.ygpa.gam.cmmn.fclty.service.GamNticRequestMngtService;
+import egovframework.rte.ygpa.gam.oper.center.service.GamMarineCenterRentArrrgMngtVO;
 import egovframework.rte.ygpa.gam.oper.center.service.GamMarineCenterRentNticMngtService;
 import egovframework.rte.ygpa.gam.oper.center.service.GamMarineCenterRentNticMngtVO;
 
@@ -28,39 +40,151 @@ import egovframework.rte.ygpa.gam.oper.center.service.GamMarineCenterRentNticMng
 
 public class GamMarineCenterRentNticMngtServiceImpl  extends AbstractServiceImpl implements GamMarineCenterRentNticMngtService {
 
+	@Resource(name="gamNticRequestMngtService")
+    private GamNticRequestMngtService gamNticRequestMngtService;
+
 	@Resource(name="gamMarineCenterRentNticMngtDao")
     private GamMarineCenterRentNticMngtDao gamMarineCenterRentNticMngtDao;
 
 	protected Log log = LogFactory.getLog(this.getClass());
 
 	/**
-	 * 마린센터임대료납부관리 목록을 조회한다.
+	 * 자산임대료납부관리 목록을 조회한다.
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return list
 	 * @exception Exception
 	 */
-    public List selectMarineCenterRentNticList(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
-        return gamMarineCenterRentNticMngtDao.selectMarineCenterRentNticList(searchVO);
+    public List selectMarineCenterRentFeePayDtlsList(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+        return gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsList(searchVO);
     }
 
     /**
-	 * 마린센터임대료납부관리 목록 총 갯수를 조회한다.
+	 * 자산임대료납부관리 목록 총 갯수를 조회한다.
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return cnt
 	 * @exception
 	 */
-    public int selectMarineCenterRentNticListTotCnt(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
-		return gamMarineCenterRentNticMngtDao.selectMarineCenterRentNticListTotCnt(searchVO);
+    public int selectMarineCenterRentFeePayDtlsListTotCnt(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+		return gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsListTotCnt(searchVO);
 	}
 
     /**
 	 * 자료수, 사용료, 부가세, 고지액을 조회한다.
 	 * @param searchVO - 조회할 정보가 담긴 VO
-	 * @return 마린센터임대료납부관리목록
+	 * @return 자산임대료납부관리목록
 	 * @exception Exception
 	 */
-    public GamMarineCenterRentNticMngtVO selectMarineCenterRentNticSum(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
-        return gamMarineCenterRentNticMngtDao.selectMarineCenterRentNticSum(searchVO);
+    public GamMarineCenterRentNticMngtVO selectMarineCenterRentFeePayDtlsSum(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+        return gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsSum(searchVO);
     }
+
+
+    /**
+	 * 고지금액합계, 수납금액합계
+	 * @param searchVO - 조회할 정보가 담긴 VO
+	 * @return vo
+	 * @exception Exception
+	 */
+    public GamMarineCenterRentNticMngtVO selectMarineCenterRentFeePayDtlsMngtSum(GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+        return gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsMngtSum(searchVO);
+    }
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentFeePayDtlsMngtService#selectNticArrrgList(egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentNticMngtVO)
+	 */
+	@Override
+	public List<?> selectNticArrrgList(GamMarineCenterRentArrrgMngtVO searchVO)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return gamMarineCenterRentNticMngtDao.selectNticArrrgList(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentFeePayDtlsMngtService#selectNticArrrgListTotCnt(egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentNticMngtVO)
+	 */
+	@Override
+	public int selectNticArrrgListTotCnt(GamMarineCenterRentArrrgMngtVO searchVO)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return gamMarineCenterRentNticMngtDao.selectNticArrrgListTotCnt(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentFeePayDtlsMngtService#mergeErpGisAssetCodeMngt(java.util.Map)
+	 */
+	@Override
+	public List mergeNticArrrgListMngt(Map mergeMap) throws Exception {
+
+        ArrayList arraylistCU = (ArrayList)mergeMap.get("CU");
+        HashMap[] hmCU = (HashMap[])arraylistCU.toArray(new HashMap[arraylistCU.size()]);
+        Map result;
+
+//        Integer photoSeq=1;
+
+//		if(hmCU.length>0) photoSeq=gamErpGisAssetPhotoMngtDao.selectGamAssetPhotoMaxSeq(hmCU[0]);
+        //수정처리 & 입력처리
+        for (int i=0; i<hmCU.length; i++) {
+        	if ("I".equals(hmCU[i].get("_updtId"))) {
+//            	log.debug("#photoeq : "+photoSeq.toString());
+            }
+        	else if("U".equals(hmCU[i].get("_updtId"))){
+        		// 연체 세입 선택 목록
+
+        	}
+            else {
+            	log.debug("unknown RowStatus ["+i+"] : "+hmCU[i].get("_updtId"));
+            }
+        }
+
+
+
+		// TODO Auto-generated method stub
+		return gamMarineCenterRentNticMngtDao.mergeNticArrrgList(mergeMap);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentFeePayDtlsMngtService#updateMarineCenterRentFeePayDtlsMngtList()
+	 */
+	@Override
+	public int updateMarineCenterRentFeePayDtlsMngtList() throws Exception {
+		return gamMarineCenterRentNticMngtDao.updateMarineCenterRentFeePayDtlsMngtList();
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentFeePayDtlsMngtService#selectMarineCenterRentFeePayDtlsMngtDetailList(egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentNticMngtVO)
+	 */
+	@Override
+	public List selectMarineCenterRentFeePayDtlsMngtDetailList(
+			GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+        return gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsMngtDetailList(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentFeePayDtlsMngtService#selectMarineCenterRentFeePayDtlsMngtDetailMstPk(egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentNticMngtVO)
+	 */
+	@Override
+	public EgovMap selectMarineCenterRentFeePayDtlsMngtDetailMstPk(
+			GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+		return gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsMngtDetailMstPk(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentFeePayDtlsMngtService#selectMarineCenterRentFeePayDtlsMngtDetailSumPk(egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentNticMngtVO)
+	 */
+	@Override
+	public EgovMap selectMarineCenterRentFeePayDtlsMngtDetailSumPk(
+			GamMarineCenterRentNticMngtVO searchVO) throws Exception {
+		return gamMarineCenterRentNticMngtDao.selectMarineCenterRentFeePayDtlsMngtDetailSumPk(searchVO);
+	}
+
+	/* (non-Javadoc)
+	 * @see egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentFeePayDtlsMngtService#selectNticArrrgDetail(egovframework.rte.ygpa.gam.asset.rent.service.GamMarineCenterRentNticMngtVO)
+	 */
+	@Override
+	public Map selectNticArrrgDetail(GamMarineCenterRentNticMngtVO searchVO)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return gamMarineCenterRentNticMngtDao.selectNticArrrgDetail(searchVO);
+	}
 
 }
