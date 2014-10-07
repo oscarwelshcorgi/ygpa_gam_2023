c8844c437da3401003f7cd25f3010f1c7e30af93
yesdaisy <yesdaisy@55a9a2a8-cf3b-443f-8c2c-fc4ace15ebcd>
2014-10-07 00:46:48 +0000

    비관리청 허가원부 총공사금액 제외 처리완료

--- src/main/java/egovframework/rte/ygpa/gam/soc/web/GamSocAgentController.java
+++ src/main/java/egovframework/rte/ygpa/gam/soc/web/GamSocAgentController.java
@@ -247,12 +247,12 @@ public class GamSocAgentController {
     			gamSocAgentService.insertSocAgentList(updateSubData);
     		}
     		map.put("resultCode", 0);			// return ok
-    		map.put("resultMsg", egovMessageSource.getMessage("success.common.update"));
+    		map.put("resultMsg", egovMessageSource.getMessage("success.common.insert"));
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 			map.put("resultCode", 1);
-			map.put("resultMsg", egovMessageSource.getMessage("fail.common.update"));
+			map.put("resultMsg", egovMessageSource.getMessage("fail.common.insert"));
 		}
 
     	return map;
@@ -357,12 +357,12 @@ public class GamSocAgentController {
     		gamSocAgentService.deleteSocAgentList(searchData);
     		
     		map.put("resultCode", 0);			// return ok
-    		map.put("resultMsg", egovMessageSource.getMessage("success.common.update"));
+    		map.put("resultMsg", egovMessageSource.getMessage("success.common.delete"));
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 			map.put("resultCode", 1);
-			map.put("resultMsg", egovMessageSource.getMessage("fail.common.update"));
+			map.put("resultMsg", egovMessageSource.getMessage("fail.common.delete"));
 		}
 
     	return map;
