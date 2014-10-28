578070ec0aa5ad628a688509f6bc105ed75cbddb
lee973 <lee973@55a9a2a8-cf3b-443f-8c2c-fc4ace15ebcd>
2014-10-28 07:15:40 +0000

    시설물 계약

---
+++ src/main/java/egovframework/rte/ygpa/gam/ctrt/web/GamFcltyCtrtLgerHistController.java
@@ -0,0 +1,79 @@
+/**
+ *
+ */
+package egovframework.rte.ygpa.gam.ctrt.web;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springmodules.validation.commons.DefaultBeanValidator;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import egovframework.com.cmm.ComDefaultVO;
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+import egovframework.rte.ygpa.gam.fclty.service.GamCivilFcltyMngtService;
+import egovframework.rte.ygpa.gam.fclty.service.GamFcltyManageVO;
+import egovframework.rte.ygpa.gam.fclty.service.GamFcltyMngtService;
+
+/**
+ *
+ * @author kok
+ * @since 2014. 2. 3.
+ * @version 1.0
+ * @see
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *   수정일 		 수정자		 수정내용
+ *  -------		--------	---------------------------
+ *  2014. 2. 3.		kok		최초 생성
+ *
+ * Copyright (C) 2013 by LFIT  All right reserved.
+ * </pre>
+ */
+
+@Controller
+public class GamFcltyCtrtLgerHistController {
+
+	/** Validator */
+	@Autowired
+	private DefaultBeanValidator beanValidator;
+
+	/** EgovPropertyService */
+    @Resource(name = "propertiesService")
+    protected EgovPropertyService propertiesService;
+
+	/** EgovMessageSource */
+    @Resource(name="egovMessageSource")
+    EgovMessageSource egovMessageSource;
+
+
+	@RequestMapping(value="/ctrt/gamFcltyCtrtLgerHist.do")
+    String indexMain(@RequestParam("window_id") String windowId, ModelMap model) throws Exception {
+
+    	model.addAttribute("windowId", windowId);
+
+    	return "/ygpa/gam/ctrt/GamFcltyCtrtMng";
+    }
+
+
+
+}
