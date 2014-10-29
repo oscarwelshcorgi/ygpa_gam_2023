9b100db74529646f96f986b5264d205bb1b98d3b
yesdaisy <yesdaisy@55a9a2a8-cf3b-443f-8c2c-fc4ace15ebcd>
2014-10-29 07:53:58 +0000

    .

--- src/main/java/egovframework/rte/ygpa/gam/ctrt/web/GamFcltyCtrtLgerHistController.java
+++ src/main/java/egovframework/rte/ygpa/gam/ctrt/web/GamFcltyCtrtLgerHistController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springmodules.validation.commons.DefaultBeanValidator;
@@ -29,14 +30,17 @@ import egovframework.com.cmm.util.EgovUserDetailsHelper;
 import egovframework.rte.fdl.property.EgovPropertyService;
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+import egovframework.rte.ygpa.gam.ctrt.service.GamFcltyCtrtLgerHistService;
+import egovframework.rte.ygpa.gam.ctrt.service.GamFcltyCtrtLgerHistVO;
 import egovframework.rte.ygpa.gam.fclty.service.GamCivilFcltyMngtService;
 import egovframework.rte.ygpa.gam.fclty.service.GamFcltyManageVO;
 import egovframework.rte.ygpa.gam.fclty.service.GamFcltyMngtService;
+import egovframework.rte.ygpa.gam.soc.service.GamSocAgentService;
 
 /**
  *
- * @author kok
- * @since 2014. 2. 3.
+ * @author HNJ
+ * @since 2014. 10. 29.
  * @version 1.0
  * @see
  * <pre>
@@ -44,7 +48,7 @@ import egovframework.rte.ygpa.gam.fclty.service.GamFcltyMngtService;
  *
  *   수정일 		 수정자		 수정내용
  *  -------		--------	---------------------------
- *  2014. 2. 3.		kok		최초 생성
+ *  2014. 10. 29.		HNJ		최초 생성
  *
  * Copyright (C) 2013 by LFIT  All right reserved.
  * </pre>
@@ -64,6 +68,9 @@ public class GamFcltyCtrtLgerHistController {
 	/** EgovMessageSource */
     @Resource(name="egovMessageSource")
     EgovMessageSource egovMessageSource;
+    
+    @Resource(name = "gamFcltyCtrtLgerHistService")
+    private GamFcltyCtrtLgerHistService gamFcltyCtrtLgerHistService;
 
 
 	@RequestMapping(value="/ctrt/gamFcltyCtrtLgerHist.do")
@@ -74,6 +81,64 @@ public class GamFcltyCtrtLgerHistController {
     	return "/ygpa/gam/ctrt/GamFcltyCtrtLgerHist";
     }
 
+	/**
+     * 계약정보목록을 조회한다.
+     *
+     * @param searchVO
+     * @return map
+     * @throws Exception the exception
+     */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+    @RequestMapping(value="/soc/gamSelectFcltyCtrtLgerHistList.do", method=RequestMethod.POST)
+	public @ResponseBody Map selectFcltyCtrtLgerHistList(GamFcltyCtrtLgerHistVO searchVO) throws Exception {
+		
+		int totalCnt, page, firstIndex;
+		long sumPlanAmt, sumPrmtAmt, sumScsbidAmt, sumBaseAmt;
+    	Map map = new HashMap();
 
+    	Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+    	if(!isAuthenticated) {
+	        map.put("resultCode", 1);
+    		map.put("resultMsg", egovMessageSource.getMessage("fail.common.login"));
+        	return map;
+    	}
+
+    	PaginationInfo paginationInfo = new PaginationInfo();
+		paginationInfo.setCurrentPageNo(searchVO.getPageIndex());
+		paginationInfo.setRecordCountPerPage(searchVO.getPageUnit());
+		paginationInfo.setPageSize(searchVO.getPageSize());
+
+		searchVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+		searchVO.setLastIndex(paginationInfo.getLastRecordIndex());
+		searchVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
+
+		
+		//계약대장목록
+    	List fcltyCtrtLgerHistList = gamFcltyCtrtLgerHistService.selectFcltyCtrtLgerHistList(searchVO);
+    	
+    	//계약대장목록 총갯수 및 금액합계
+		GamFcltyCtrtLgerHistVO fcltyCtrtLgerHistInfoSum = gamFcltyCtrtLgerHistService.selectFcltyCtrtLgerHistInfoSum(searchVO);
+    	
+		totalCnt = fcltyCtrtLgerHistInfoSum.getTotalCnt();
+		sumPlanAmt = fcltyCtrtLgerHistInfoSum.getSumPlanAmt();
+		sumPrmtAmt = fcltyCtrtLgerHistInfoSum.getSumPrmtAmt();
+		sumScsbidAmt = fcltyCtrtLgerHistInfoSum.getSumScsbidAmt();
+		sumBaseAmt = fcltyCtrtLgerHistInfoSum.getSumBaseAmt();
+    	
+    	paginationInfo.setTotalRecordCount(totalCnt);
+        searchVO.setPageSize(paginationInfo.getLastPageNoOnPageList());
+        
+ 
+    	map.put("resultCode", 0);	// return ok
+    	map.put("totalCount", totalCnt);
+    	map.put("sumPlanAmt", sumPlanAmt);
+    	map.put("sumPrmtAmt", sumPrmtAmt);
+    	map.put("sumScsbidAmt", sumScsbidAmt);
+    	map.put("sumBaseAmt", sumBaseAmt);
+    	map.put("resultList", fcltyCtrtLgerHistList);
+    	map.put("searchOption", searchVO);
+
+    	return map;
+    }
 
 }
